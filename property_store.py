9fb83289bb47124ec1d1fd1a65ba74e7cb1eca40
dancerdevin <>
2026-02-05 12:47:59 -0800

    Working assembly in which PropertyStore.get() calls CompletePropertyProvider.request() and at the end, we have a Property object. Because I am relying on my old code, functions that should return a partial Property are still returning a DataFrame. But this clarifies that, to make this system modular, I want to save all analysis for the end: that is, get all the data I want to keep, put it in the Property, keep going and adding to the Property, and then separately, take all that data and perform whatever analysis functions I want to do once all external data intake is complete and the info is all available. That makes more sense generally and will be way more flexible as I start to bring in different external API data sources at different points.

--- property_store.py
+++ property_store.py
@@ -1,6 +1,7 @@
 from property_schema import Property
 from typing import Protocol
 from property_data_intake import rentcast_api, rentometer_api
+from property_level_analysis import parse_rentcast_data, add_rent_to_parsed_rentcast_data, add_costs_to_parsed_rentcast_data, build_attributes, build_features, build_location, build_metadata, build_values
 
 """Store and retrieve Property objects, calling intake APIs when needed data is not already stored."""
 
@@ -26,30 +27,53 @@ class PropertyProvider(Protocol):
 
 class CompletePropertyProvider:
   # List distinct PropertyProviders and go through them as needed. This will return a completely initialized Property object.
-  def request(location) -> Property:
+  def request(self, location) -> Property:
     prop = Property()
     rentcast_provider = RentcastPropertyProvider()
-    prop = rentcast_provider.request(location)
+    # TODO: currently a df, not a Property
+    rentcast_df = rentcast_provider.request(location)
     rentometer_provider = RentometerPropertyProvider()
-    prop = rentometer_provider.request(location)
+    rentometer_df = rentometer_provider.request(location)
+    df = add_rent_to_parsed_rentcast_data(rentcast_df, rentometer_df)
+    df = add_costs_to_parsed_rentcast_data(df)
+    # NOTE: currently copied from property_level_analysis.build_properties()
+    prop_list = []
+
+    for row in df.itertuples(index=False):
+        location_details = build_location(row)
+        feature_details = build_features(row)
+        attribute_details = build_attributes(row)
+        value_details = build_values(row)
+        metadata = build_metadata(row)
+        prop_list.append(Property(location_details, feature_details, attribute_details, value_details, metadata))
+
+    print(f"Prop_list: {prop_list}")
+
+    prop = prop_list[0]
     if not prop.is_complete():
       raise Exception("Error: Property obj is not complete at the end of CompletePropertyProvider assembly.")
     return prop
 
 class RentcastPropertyProvider:
   # This follows the PropertyProvider Protocol and def request() outputs a partial property object. Put in CompletePropertyProvider
-  def request(location) -> Property:
+  # TODO: CURRENTLY RETURNS DF, NOT PARTIAL PROPERTY. So first, do work needed with DF for a given API call, add to Property, and AT THE END, analysis.
+  def request(self, location) -> Property:
     # TODO: Consider how to use rentcast api here and how to replace build_properties() functionality with new partial objects
     df_from_json = rentcast_api(location, output="from_json_dump")
     # New functionality to build a partial property from Rentcast data specifically
-    pass
+    rentcast_subset_df = parse_rentcast_data(df_from_json)
+    return rentcast_subset_df
 
 class RentometerPropertyProvider:
-  def request(location) -> Property:
+  # TODO: CURRENTLY RETURNS DF, NOT PARTIAL PROPERTY. 
+  def request(self, location) -> Property:
     # TODO: Consider how to use rentcast api here and how to replace build_properties() functionality with new partial objects
     df_from_json = rentometer_api(location, output="from_json_dump")
     # New functionality to build a partial property from Rentometer data specifically
-    pass
+    return df_from_json
 
 if __name__ == "__main__":
-  pass
+  # Test address: '5214 S Thompson Ave, Tacoma, WA 98408'
+  prop_store = PropertyStore()
+  prop = prop_store.get("5214 S Thompson Ave, Tacoma, WA 98408")
+  print(prop)
